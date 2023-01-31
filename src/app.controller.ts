0851d987d657e99133c9eb20c875c9dd66136bbd
FZsiga02 <fzsigmond02@gmail.com>
2023-01-31 08:44:10 +0100

    Csavarok listázása

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,32 @@
-import { Body, Controller, Get, Post, Render } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body,
+         Controller,
+         Delete,
+         Get,
+         Param,
+         Post,
+         Put,
+         Render
+     } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { AppService } from './app.service';
+import { Csavarbolt } from './csavarbolt.entity';
+import { Rendeles } from './rendeles.entity';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private dataSource: DataSource,
-  ) {}
+    private readonly appService: AppService;
+    private dataSource: DataSource;
 
   @Get()
   @Render('index')
   index() {
     return { message: 'Welcome to the homepage' };
+  }
+
+  @Get('/csavar')
+  listCsavarok() {
+    const csavarRepo = this.dataSource.getRepository(Csavarbolt);
+    return csavarRepo.find();
   }
 }
